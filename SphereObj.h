ad2f0910c20563df8734b7b6863c7c0116a12183
quanghoa281 <quanghoa281@gmail.com>
2022-07-04 21:57:41 +0700

    add  new func

--- SphereObj.h
+++ SphereObj.h
@@ -7,10 +7,6 @@ private:
 	int cradius;
 	GLuint g_obj;
 public:
-	/*SphereObj(int radius)
-	: cradius(radius)
-	{
-	}*/
 	void make(int radius);
 	GLuint get();
 };
@@ -24,7 +20,6 @@ private:
 	int cradius;
 	GLuint g_obj;
 public:
-
 	void make(const float length, const float width, const float height);
 	GLuint get();
 };
